2d2e9b3c4125401ba610bb81c1278eae06b7f70f
Wuuzzaa <jonas-licht@gmx.de>
2024-06-15 14:11:13 +0200

    add config.py add EXCEL_OUTPUT_PATH

---
+++ config.py
@@ -0,0 +1,5 @@
+# constants
+CSV_REPORT_PATH = "data/report.csv"
+CSV_USD_EUR_PATH = "data/usd_eur.csv"
+OPTION_DATA_INDICATOR_STRING = "Aktien- und Indexoptionen"
+EXCEL_OUTPUT_PATH = "data/output_excel.xlsx"
